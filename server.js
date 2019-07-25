4b95c66c325f6f520ec54a9d447fbec7075adfba
Nicholas Loperena <nicholasloperena@gmail.com>
2019-07-24 22:35:51 -0400

    succesfully scraped information on the article name, link, and imagelink from floridaman.com

---
+++ server.js
@@ -0,0 +1,40 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+
+console.log("\n***********************************\n" +
+            "Grabbing every story about Florida Man\n" +
+            "from floridaman.com :" +
+            "\n***********************************\n");
+
+
+axios.get("https://floridaman.com/").then(function(response) {
+
+ 
+  var $ = cheerio.load(response.data);
+
+  
+  var results = [];
+
+  
+  $("article.entry-tpl-grid").each(function(i, element) {
+
+    
+    var article = $(element).find("h3").text();
+
+    var link = $(element).find("a").attr("href");
+  
+    var imgLink = $(element).find("a").find("img").attr("data-srcset").split(",")[0].split(" ")[0];
+
+
+   
+    results.push({
+      article: article,
+      link: link,
+      imgLink: imgLink
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
